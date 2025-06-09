55d43bd0ceed11dd131672ac9e0bdd64c57a27c1
Gustavo Guilherme <gustavo.gsilva1723@gmail.com>
2025-06-09 00:50:29 -0300

    refact: remove assert in swagger router import

--- src/routes/swaggerRoute.js
+++ src/routes/swaggerRoute.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 import swaggerUI from "swagger-ui-express";
-import swaggerDocument from "./../../swagger.json" assert { type: "json" };
+import swaggerDocument from "./../../swagger.json"
 
 router.use("/", swaggerUI.serve);
 router.get("/", swaggerUI.setup(swaggerDocument));
